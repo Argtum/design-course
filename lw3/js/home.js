880dabfd2b046e5a1e9d169f696a64a3d69d1bca
Aleksey Serebruakov <Silwar40k@gmail.com>
2018-09-28 02:08:21 +0300

    remade, but not work

--- lw3/js/home.js
+++ lw3/js/home.js
@@ -1,47 +1,12 @@
+function Sun({startX, startY, moveSpeed}) {
+    this.x = startX;
+    this.y = startY;
+    this.moveSpeed = moveSpeed;
+}
+
 function drawBackground(ctx, canvasWidth) {
     ctx.fillStyle = 'rgb(0, 148, 255)';
     ctx.fillRect(0, 0, canvasWidth, 400);
-}
-
-function update({ctx, sunX, sunY}) {
-    moveSun({ctx, sunX, sunY});
-}
-
-function redraw({ctx, width}) {
-    drawBackground(ctx, width);
-    drawClouds(ctx);
-    drawFrontground(ctx, width);
-}
-
-function moveSun(ctx, speed, radius, centerX, centerY, width) {
-    const circle = 60;// радиан
-    for (let distance = 1; distance < circle; distance++)
-    {
-        let distance = 1;
-
-        let sunX = Math.cos(speed * distance) * radius + centerX;
-        let sunY = Math.sin(speed * distance) * radius + centerY;
-        // drawBackground(ctx, width);
-        // drawClouds(ctx);
-        // drawFrontground(ctx, width);
-        // drawSun(ctx, sunX, sunY);
-        if (distance == circle - 1) {
-            distance = 0;
-        }
-    }
-}
-
-function drawSun(ctx, posX, posY) {
-    //sun
-    ctx.beginPath();
-    ctx.arc(posX, posY, 50, 0, Math.PI * 2);
-    ctx.fillStyle = '#FFEA93';
-    ctx.fill();
-
-    //cloud
-    drawCloud(ctx, 500, 70);
-    drawCloud(ctx, 200, 150);
-    drawCloud(ctx, 800, 130);
 }
 
 function drawCloudPart(ctx, startX, startY, radiusX, radiusY, color) {
@@ -57,8 +22,7 @@ function drawCloud(ctx, startX, startY) {
     drawCloudPart(ctx, startX + 35, startY + 10, 50, 25, '#C8E0F4');
 }
 
-function drawClouds(ctx, canvasWidth) {
-    //cloud
+function drawClouds(ctx) {
     drawCloud(ctx, 500, 70);
     drawCloud(ctx, 200, 150);
     drawCloud(ctx, 800, 130);
@@ -76,12 +40,12 @@ function drawFrontground(ctx, canvasWidth) {
     ctx.fillStyle = '#666666';
     ctx.fillRect(570, 175, 30, 100);
 
+    ctx.strokeStyle = '#5E5D59';
     ctx.beginPath();
     ctx.moveTo(380, 275);
     ctx.lineTo(510, 140);
     ctx.lineTo(640, 275);
     ctx.lineTo(380, 275);
-    ctx.strokeStyle = '#5E5D59';
     ctx.stroke();
     ctx.fillStyle = '#E60000';
     ctx.fill();
@@ -89,6 +53,7 @@ function drawFrontground(ctx, canvasWidth) {
     ctx.fillStyle = '#FFDF5C';
     ctx.fillRect(470, 310, 80, 100);
 
+    ctx.strokeStyle = '#5E5D59';
     ctx.beginPath();
     ctx.moveTo(470, 310);
     ctx.lineTo(550, 310);
@@ -99,22 +64,54 @@ function drawFrontground(ctx, canvasWidth) {
     ctx.lineTo(510, 410);
     ctx.moveTo(470, 360);
     ctx.lineTo(550, 360);
-    ctx.strokeStyle = '#5E5D59';
     ctx.stroke();
 }
 
-function draw() {
+function drawSun(ctx, sun) {
+    ctx.fillStyle = '#FFEA93';
+    ctx.beginPath();
+    ctx.arc(sun.x, sun.y, 50, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+function redraw(ctx, width, sun) {
+    drawBackground(ctx, width);
+    drawClouds(ctx);
+    drawFrontground(ctx, width);
+    drawSun(ctx, sun)
+}
+
+function moveSun({sun, speed, radius, centerX, centerY, dt}) {
+    const distance = sun.moveSpeed * dt;
+    sun.x += Math.cos(speed * distance) * radius + centerX;
+    syn.y += Math.sin(speed * distance) * radius + centerY;
+}
+
+function update({sun, speed, radius, centerX, centerY, dt}) {
+    moveSun({sun, speed, radius, centerX, centerY, dt});
+}
+
+function createSun() {
+    const startX = 500;
+    const startY = 400;
+    const moveSpeed = 1; //радиан
+
+    return new Sun({
+        startX,
+        startY,
+        moveSpeed
+    });
+}
+
+function main() {
     const canvas = document.getElementById('canvas');
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
+    const width = canvas.offsetWidth;
+    const height = canvas.offsetHeight;
     const ctx = canvas.getContext('2d');
     const radius = 300;
-    const speed = 1; //радиан
-    const centerX = 500;
-    const centerY = 400;
 
-    // moveSun(ctx, speed, radius, centerX, centerY, canvas.width);
-    redraw({ctx, width});
+    createSun();
+    redraw(ctx, width, sun);
 
     let lastTimestamp = Date.now(); //текущее время в ms
     const animateFn = () => {
@@ -122,14 +119,10 @@ function draw() {
         const deltaTime = (currentTimeStamp - lastTimestamp) * 0.001; //сколько секунд прошло с прошлого кадра
         lastTimestamp = currentTimeStamp;
 
-        update({ctx, sunX, sunY});
-        redraw({ctx, sunX, sunY, width});
+        update({sun, speed, radius, centerX, centerY, dt: deltaTime});
+        redraw(ctx, width, sun);
         requestAnimationFrame(animateFn);
-        }
+    }
 
     animateFn();
 }
-
-window.onload = function() {
-    draw();
-};
